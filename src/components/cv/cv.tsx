c70411640808fef1c85457ca4945dc2600804615
Jebu04 <jebu.meme@gmail.com>
2022-03-06 20:10:58 +0200

    Changes

--- src/components/cv/cv.tsx
+++ src/components/cv/cv.tsx
@@ -10,12 +10,7 @@ const Cv: React.FC<CvProps> = () => {
   return (
         <div className={styles.cv}>
             <h1 className="cv">CV</h1>
-            <div className={styles.cvLink}>
-              <h2 className={styles.cvHeader}>
-                Jesse Nordman
-              </h2>
-              <p>21.1.2004</p>
-              <p> +358 458899111 </p>
+              <div className={styles.cvLink}>
                 <a href={cv} download="Jesse-Nordman-CV">
                         Download CV
                 </a>
